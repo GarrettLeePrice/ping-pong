de5b797625e4008bbc2aab7c4830fe3423ee52d5
Jon Toler <tolerjonathan@gmail.com>
2016-08-24 12:33:37 -0700

    fix front-end/back-end logic separation, add code to empty results from previous input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,37 @@
-var outputNums = [];
 
 function pingPong(number) {
-for (var i = 1; i <= number; i++) {
-  if (i % 3 === 0) {
-    outputNums.push("ping");
-  } else if (i % 5 === 0) {
-    outputNums.push("pong");
-  } else if (i % 3 ===0 && i % 5 === 0) {
-    outputNums.push("ping pong");
-  } else {outputNums.push(i);
-  };
-};
-  for (var i = 0; i < outputNums.length; i++) {
-    $("ul").append("<li>" + outputNums[i] + "</li>");
+  var outputNums = [];
+  for (var i = 1; i <= number; i++) {
+    if (i % 3 ===0 && i % 5 === 0) {
+      outputNums.push("ping pong");
+    }  else if (i % 3 === 0) {
+      outputNums.push("ping");
+    } else if (i % 5 === 0) {
+      outputNums.push("pong");
+    } else
+      outputNums.push(i);
+    }
+    return outputNums;
   }
-};
+
+
+
 
 
 
 
 $(document).ready(function() {
-  $("#form").submit(function(event) {
-  var input = $("#input").val();
-  pingPong(input);
+
+  $("form").submit(function(event) {
+    $("ul").empty();
+  var input = parseInt($("#input").val());
+  if (input) {
+    var outputNums = pingPong(input)
+    for (var i = 0; i < outputNums.length; i++) {
+    $("ul").append("<li>" + outputNums[i] + "</li>");
+
+  }
   event.preventDefault();
-  });
+};
+});
 });
