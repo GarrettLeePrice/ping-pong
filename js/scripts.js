ad6096617fe7fa008935c8047f6ee229363b67a8
Garrett Price <garrettleeprice@gmail.com>
2016-08-24 19:42:21 -0700

    fix css issues & fix spacing and indentation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,26 @@
-
 function pingPong(number) {
   var outputNums = [];
   for (var i = 1; i <= number; i++) {
-    if (i % 3 ===0 && i % 5 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
       outputNums.push("ping pong");
-    }  else if (i % 3 === 0) {
+    } else if (i % 3 === 0) {
       outputNums.push("ping");
     } else if (i % 5 === 0) {
       outputNums.push("pong");
-    } else
-      outputNums.push(i);
-    }
-    return outputNums;
+    } else outputNums.push(i);
   }
-
-
-
-
-
-
-
+  return outputNums;
+}
 $(document).ready(function() {
-
   $("form").submit(function(event) {
     $("ul").empty();
-  var input = parseInt($("#input").val());
-  if (input) {
-    var outputNums = pingPong(input)
-    for (var i = 0; i < outputNums.length; i++) {
-    $("ul").append("<li>" + outputNums[i] + "</li>");
-
-  }
-  event.preventDefault();
-};
-});
+    var input = $("#input").val();
+    if (input) {
+      var outputNums = pingPong(input)
+      for (var i = 0; i < outputNums.length; i++) {
+        $("ul").append("<li>" + outputNums[i] + "</li>");
+      }
+      event.preventDefault();
+    };
+  });
 });
